b1d3d5c8bc17ff376c01763d4973b5cf562779ec
JB <39542105+spac3P1rat3@users.noreply.github.com>
2023-05-25 12:29:50 -0500

    Update app.py with more in depth api key testing.
    Reasoning is if you have api key issues it won't show the issue easily.

--- app.py
+++ app.py
@@ -1,55 +1,76 @@
 from dotenv import load_dotenv
 import streamlit as st
-from PyPDF2 import PdfReader
+import os
+import openai
+from PyPDF2 import PdfReader 
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
-
+from langchain.chains.question_answering import load_qa_chain
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
-    st.header("Ask your PDF 💬")
-    
-    # upload file
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
-    
-    # extract the text
-    if pdf is not None:
-      pdf_reader = PdfReader(pdf)
-      text = ""
-      for page in pdf_reader.pages:
-        text += page.extract_text()
-        
-      # split into chunks
-      text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-      )
-      chunks = text_splitter.split_text(text)
-      
-      # create embeddings
-      embeddings = OpenAIEmbeddings()
-      knowledge_base = FAISS.from_texts(chunks, embeddings)
-      
-      # show user input
-      user_question = st.text_input("Ask a question about your PDF:")
-      if user_question:
-        docs = knowledge_base.similarity_search(user_question)
-        
-        llm = OpenAI()
-        chain = load_qa_chain(llm, chain_type="stuff")
-        with get_openai_callback() as cb:
-          response = chain.run(input_documents=docs, question=user_question)
-          print(cb)
-           
-        st.write(response)
-    
+    st.header("Ask your PDF")
 
-if __name__ == '__main__':
+    # Upload PDF
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+    # Extract text from PDF
+    if pdf is not None:
+        pdf_read = PdfReader(pdf)
+        text = ""
+        for page in pdf_read.pages:
+            text += page.extract_text()
+        
+        # Split text into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+
+        chunks = text_splitter.split_text(text)
+        api_key = os.getenv("OPENAI_API_KEY")
+
+        # Check if api exists
+        if api_key is None:
+            st.error("Please set OPENAI_API_KEY in .env file")
+            return
+        # Check if the api key is valid by sending a simple 'test' prompt
+        try:
+            openai.api_key = api_key
+            st.write("Testing OPENAI_API_KEY...")
+            list = openai.Engine.list()
+            
+        except Exception as e:
+            st.write(e)
+            st.error("Invalid OPENAI_API_KEY")
+            return
+        
+        if not list is None: 
+            st.success("Valid OPENAI_API_KEY: "+ str(len(list.data)) + " engines found")
+
+        # Create embeddings
+        embeddings = OpenAIEmbeddings()
+        embeddings.openai_api_key = api_key
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        # Ask a question
+
+        user_question = st.text_input("Ask a question about the PDF:")
+        if user_question is not None and user_question != "":
+            st.write("Searching for answer...")
+            docs = knowledge_base.similarity_search(user_question)
+            
+            if len(docs) > 0:
+                llm = OpenAI()
+                chain = load_qa_chain(llm, chain_type="stuff")
+                response = chain.run(input_documents=docs, question=user_question)
+                st.write(response)
+            else:
+                st.error("No answer found")
+if __name__ == "__main__":
     main()
